2c19a298b1c9d30bf90f427bb74925b1f0091a21
Andy <amueller@ais.uni-bonn.de>
2012-05-20 18:18:31 +0200

    Adjusted middlebury example

--- example_middlebury.py
+++ example_middlebury.py
@@ -1,8 +1,7 @@
 import Image
 import matplotlib.pyplot as plt
 import numpy as np
-from gco import cut_simple
-from daicrf import potts_mrf
+from pygco import cut_simple
 
 
 def stereo_unaries(img1, img2):
@@ -17,29 +16,21 @@ def stereo_unaries(img1, img2):
         if disp != max_disp - 1:
             diff = diff[:, max_disp - disp - 1:disp - max_disp + 1]
         differences.append(diff)
-    return np.dstack(differences)
+    return np.dstack(differences).copy("C")
 
 
 def potts_example():
     img1 = np.asarray(Image.open("scene1.row3.col1.ppm")) / 255.
     img2 = np.asarray(Image.open("scene1.row3.col2.ppm")) / 255.
-    unaries = stereo_unaries(img1, img2)
+    unaries = (stereo_unaries(img1, img2) * 100).astype(np.int32)
     n_disps = unaries.shape[2]
 
     newshape = unaries.shape[:2]
-    potts_cut = cut_simple(unaries.shape[0], unaries.shape[1],
-            unaries.reshape(-1, n_disps), -5 * np.eye(n_disps))
+    potts_cut = cut_simple(unaries, -5 * np.eye(n_disps, dtype=np.int32))
     x, y = np.ogrid[:n_disps, :n_disps]
-    one_d_topology = np.abs(x - y).astype(np.float)
+    one_d_topology = np.abs(x - y).astype(np.int32).copy("C")
 
-    one_d_cut = cut_simple(unaries.shape[0], unaries.shape[1],
-            unaries.reshape(-1, n_disps), 5 * one_d_topology)
-    # build edges for max product inference:
-    inds = np.arange(np.prod(newshape)).reshape(newshape)
-    horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
-    vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
-    edges = np.vstack([horz, vert])
-    max_product = potts_mrf(np.exp(unaries.reshape(-1, n_disps)), edges, 10)
+    one_d_cut = cut_simple(unaries, 5 * one_d_topology)
     plt.subplot(231)
     plt.imshow(img1)
     plt.subplot(232)
@@ -50,8 +41,6 @@ def potts_example():
     plt.imshow(potts_cut.reshape(newshape), interpolation='nearest')
     plt.subplot(235)
     plt.imshow(one_d_cut.reshape(newshape), interpolation='nearest')
-    plt.subplot(236)
-    plt.imshow(max_product.reshape(newshape), interpolation='nearest')
     plt.show()
 
 potts_example()
