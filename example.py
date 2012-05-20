eddbe0850f7f6d213b52540004be3e823f9cab8e
Andy <amueller@ais.uni-bonn.de>
2012-05-20 17:49:34 +0200

    Examples working with Cython version

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-#from _gco_python import cut_simple, cut_from_graph
-from pygco import cut_simple
+from pygco import cut_simple, cut_from_graph
 
 
 def example_binary():
@@ -17,8 +16,19 @@ def example_binary():
     unaries = (10 * np.dstack([unaries, -unaries]).copy("C")).astype(np.int32)
     # create potts pairwise
     pairwise = -10 * np.eye(2, dtype=np.int32)
+
     # do simple cut
     result = cut_simple(unaries, pairwise)
+
+    # use the gerneral graph algorithm
+    # first, we construct the grid graph
+    inds = np.arange(x.size).reshape(x.shape)
+    horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
+    vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
+    edges = np.vstack([horz, vert]).astype(np.int32)
+
+    # we flatten the unaries
+    result_graph = cut_from_graph(edges, unaries.reshape(-1, 2), pairwise)
 
     # plot results
     plt.subplot(231, title="original")
@@ -31,49 +41,43 @@ def example_binary():
     plt.imshow(x_thresh, interpolation='nearest')
     plt.subplot(235, title="cut_simple")
     plt.imshow(result, interpolation='nearest')
+    plt.subplot(236, title="cut_from_graph")
+    plt.imshow(result_graph.reshape(x.shape), interpolation='nearest')
 
-    #inds = np.arange(x.size).reshape(x.shape)
-    #horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
-    #vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
-    #edges = np.vstack([horz, vert])
-    #result = cut_from_graph(edges.tolist(), unaries, 1.1 * np.eye(2))
-
-    #result_mrf = cut_from_graph(edges.tolist(), unaries,  np.eye(2) * 1.1)
-    #plt.matshow(result_mrf.reshape(x.shape), interpolation='nearest')
     plt.show()
 
 
-#def example_multinomial():
-    #np.random.seed(45)
-    #unaries = np.zeros((10, 12, 3))
-    #unaries[:, :4, 0] = 1
-    #unaries[:, 4:8, 1] = 1
-    #unaries[:, 8:, 2] = 1
-    #x = np.argmax(unaries, axis=2)
-    #unaries_noisy = unaries + np.random.normal(size=unaries.shape)
-    #x_thresh = np.argmax(unaries_noisy, axis=2)
-    #unaries_noisy = unaries_noisy.reshape(-1, 3)
+def example_multinomial():
+    # generate dataset with three stripes
+    np.random.seed(15)
+    x = np.zeros((10, 12, 3))
+    x[:, :4, 0] = -1
+    x[:, 4:8, 1] = -1
+    x[:, 8:, 2] = -1
+    unaries = x + 1.5 * np.random.normal(size=x.shape)
+    x = np.argmin(x, axis=2)
+    unaries = (unaries * 10).astype(np.int32)
+    x_thresh = np.argmin(unaries, axis=2)
 
-    #inds = np.arange(x.size).reshape(x.shape)
-    #horz = np.c_[inds[:, :-1].ravel(), inds[:, 1:].ravel()]
-    #vert = np.c_[inds[:-1, :].ravel(), inds[1:, :].ravel()]
-    #edges = np.vstack([horz, vert])
-    #result = cut_from_graph(unaries_noisy, edges, 1.1 * np.ones(3))
-    #binaries = np.eye(3) + np.ones((1, 1))
-    #binaries[-1, 0] = 0
-    #binaries[0, -1] = 0
-    #print(binaries)
-    #result_mrf = cut_from_graph(unaries_noisy, edges, binaries)
-    #plt.subplot(141)
-    #plt.imshow(x, interpolation="nearest")
-    #plt.subplot(142)
-    #plt.imshow(x_thresh, interpolation="nearest")
-    #plt.subplot(143)
-    #plt.imshow(result.reshape(x.shape), interpolation="nearest")
-    #plt.subplot(144)
-    #plt.imshow(result_mrf.reshape(x.shape), interpolation="nearest")
-    #plt.show()
+    # potts potential
+    pairwise_potts = -2 * np.eye(3, dtype=np.int32)
+    result = cut_simple(unaries, 10 * pairwise_potts)
+    # potential that penalizes 0-1 and 1-2 less thann 0-2
+    pairwise_1d = -15 * np.eye(3, dtype=np.int32) - 8
+    pairwise_1d[-1, 0] = 0
+    pairwise_1d[0, -1] = 0
+    print(pairwise_1d)
+    result_1d = cut_simple(unaries, pairwise_1d)
+    plt.subplot(141, title="original")
+    plt.imshow(x, interpolation="nearest")
+    plt.subplot(142, title="thresholded unaries")
+    plt.imshow(x_thresh, interpolation="nearest")
+    plt.subplot(143, title="potts potentials")
+    plt.imshow(result, interpolation="nearest")
+    plt.subplot(144, title="1d topology potentials")
+    plt.imshow(result_1d, interpolation="nearest")
+    plt.show()
 
 
 example_binary()
-#example_multinomial()
+example_multinomial()
--- gco_python.pyx
+++ gco_python.pyx
@@ -13,13 +13,30 @@ cdef extern from "GCoptimization.h":
         void swap(int n_iterations)
         int whatLabel(int node)
 
-def cut_simple(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost, np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, n_iter=5, algorithm='expansion'):
+    cdef cppclass GCoptimizationGeneralGraph:
+        GCoptimizationGeneralGraph(int n_vertices, int n_labels)
+        void setDataCost(int *)
+        void setSmoothCost(int *)
+        void setNeighbors(int, int)
+        void expansion(int n_iterations)
+        void swap(int n_iterations)
+        int whatLabel(int node)
+
+
+def cut_simple(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, n_iter=5,
+        algorithm='expansion'):
+
     if data_cost.shape[2] != smoothness_cost.shape[0]:
-        raise ValueError("data_cost and smoothness_cost have incompatible shapes.\n. data_cost must be height x width x n_labels, smoothness_cost must be n_labels x n_labels.")
+        raise ValueError("data_cost and smoothness_cost have incompatible shapes.\n"
+            "data_cost must be height x width x n_labels, smoothness_cost must be n_labels x n_labels.\n"
+            "Got: data_cost: (%d, %d, %d), smoothness_cost: (%d, %d)"
+            %(data_cost.shape[0], data_cost.shape[1], data_cost.shape[2],
+                smoothness_cost.shape[0], smoothness_cost.shape[1]))
     if smoothness_cost.shape[1] != smoothness_cost.shape[0]:
         raise ValueError("smoothness_cost must be a square matrix.")
-    cdef int h = data_cost.shape[0]
-    cdef int w = data_cost.shape[1]
+    cdef int h = data_cost.shape[1]
+    cdef int w = data_cost.shape[0]
     cdef int n_labels = smoothness_cost.shape[0]
 
     cdef GCoptimizationGridGraph* gc = new GCoptimizationGridGraph(h, w, n_labels)
@@ -31,10 +48,45 @@ def cut_simple(np.ndarray[np.int32_t, ndim=3, mode='c'] data_cost, np.ndarray[np
         gc.expansion(n_iter)
 
     cdef np.npy_intp result_shape[2]
-    result_shape[0] = h
-    result_shape[1] = w
+    result_shape[0] = w
+    result_shape[1] = h
     cdef np.ndarray[np.int32_t, ndim=2] result = np.PyArray_SimpleNew(2, result_shape, np.NPY_INT32)
     cdef int * result_ptr = <int*>result.data
     for i in xrange(w * h):
         result_ptr[i] = gc.whatLabel(i)
-    return result.reshape(w, h)
+    return result
+
+
+def cut_from_graph(np.ndarray[np.int32_t, ndim=2, mode='c'] edges,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] data_cost,
+        np.ndarray[np.int32_t, ndim=2, mode='c'] smoothness_cost, n_iter=5,
+        algorithm='expansion'):
+
+    if data_cost.shape[1] != smoothness_cost.shape[0]:
+        raise ValueError("data_cost and smoothness_cost have incompatible shapes.\n"
+            "data_cost must be height x width x n_labels, smoothness_cost must be n_labels x n_labels.\n"
+            "Got: data_cost: (%d, %d), smoothness_cost: (%d, %d)"
+            %(data_cost.shape[0], data_cost.shape[1],
+                smoothness_cost.shape[0], smoothness_cost.shape[1]))
+    if smoothness_cost.shape[1] != smoothness_cost.shape[0]:
+        raise ValueError("smoothness_cost must be a square matrix.")
+    cdef int n_vertices = data_cost.shape[0]
+    cdef int n_labels = smoothness_cost.shape[0]
+
+    cdef GCoptimizationGeneralGraph* gc = new GCoptimizationGeneralGraph(n_vertices, n_labels)
+    for e in edges:
+        gc.setNeighbors(e[0], e[1])
+    gc.setDataCost(<int*>data_cost.data)
+    gc.setSmoothCost(<int*>smoothness_cost.data)
+    if algorithm == 'swap':
+        gc.swap(n_iter)
+    elif algorithm == 'expansion':
+        gc.expansion(n_iter)
+
+    cdef np.npy_intp result_shape[1]
+    result_shape[0] = n_vertices
+    cdef np.ndarray[np.int32_t, ndim=1] result = np.PyArray_SimpleNew(1, result_shape, np.NPY_INT32)
+    cdef int * result_ptr = <int*>result.data
+    for i in xrange(n_vertices):
+        result_ptr[i] = gc.whatLabel(i)
+    return result
