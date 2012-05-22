0ff7a979cd76c6fe09e267939bf45469399f6619
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-05-22 15:31:57 +0200

    Easier building

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-gco_directory = "../gco/"
+gco_directory = "../../tools/gco/"
+
+files = ['GCoptimization.cpp', 'graph.cpp', 'LinkedBlockList.cpp',
+    'maxflow.cpp']
+
+files = [gco_directory + f for f in files]
+files.insert(0, "gco_python.pyx")
 
 setup(cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("pygco", ["gco_python.pyx"], language="c++",
-        include_dirs=[gco_directory], library_dirs=[gco_directory],
-        libraries=['gco'])])
+    ext_modules=[Extension("pygco", files, language="c++",
+        include_dirs=[gco_directory], library_dirs=[gco_directory])])
