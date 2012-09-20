aa671c74c97781ecdeb885cdfe12b120832e4f31
Andreas Mueller <amueller@ais.uni-bonn.de>
2012-09-20 13:50:43 +0100

    fixed cross-platform path.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
 import numpy
 
-gco_directory = "gco_src/"
+gco_directory = "gco_src"
 
 files = ['GCoptimization.cpp', 'graph.cpp', 'LinkedBlockList.cpp',
     'maxflow.cpp']
 
-files = [gco_directory + f for f in files]
+files = [os.path.join(gco_directory, f) for f in files]
 files.insert(0, "gco_python.pyx")
 
 setup(cmdclass={'build_ext': build_ext},
